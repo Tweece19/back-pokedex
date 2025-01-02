65b924fed8daacef43c8ede4b6f84f96087c5751
Amandine Piganiol <amandine15.piganiol@epita.fr>
2025-01-03 00:48:15 +0100

    change url for prod platform 2 and change port

--- front/src/app/constant/request.url.constant.ts
+++ front/src/app/constant/request.url.constant.ts
@@ -1,4 +1,4 @@
-export const URL = _NGX_ENV_.NG_APP_URL;
-// export const URL = "http://localhost:3000";
+//export const URL = _NGX_ENV_.NG_APP_URL;
+export const URL = "http://localhost:3000";
 console.log("url", URL);
 export const POKEMONS = "pokemons";
